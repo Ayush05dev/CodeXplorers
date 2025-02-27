92c961503b4af594876addfc8c226dbdf967cbe4
Ayush-8955 <ayush058955@gmail.com>
2025-02-27 14:19:49 +0530

    commits

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import PitchEvaluation from './pages/PitchEvaluation';
+import Matchmaking from './pages/Matchmaking';
+import BusinessPlan from './pages/BusinessPlan';
+import InvestorDashboard from './pages/InvestorDashboard';
+import EntrepreneurDashboard from './pages/EntrepreneurDashboard';
+import CreateStartup from './pages/CreateStartup';
+import InvestorPreference from './pages/InvestorPreference'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-  <h1 className='text-gray-500 text-2xl'>Hi</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/investordashboard" element={<InvestorDashboard />} />
+        <Route path="/entrepreneurdashboard" element={<EntrepreneurDashboard />} />
+        <Route path="/pitch-evaluation" element={<PitchEvaluation />} />
+        <Route path="/matchmaking" element={<Matchmaking />} />
+        <Route path="/business-plan" element={<BusinessPlan />} />
+        <Route path="/createstartup" element={<CreateStartup />} />
+        <Route path="/investorpreference" element={<InvestorPreference/>} />
+      
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
